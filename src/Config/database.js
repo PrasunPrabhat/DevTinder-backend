d23c22c8c36acd2c78ec4b3f410c55d75f4883ad
PrasunPrabhat <prasun2000kir@gmail.com>
2025-08-31 17:54:35 +0530

    first commit

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,10 +1,9 @@
 // ! 1. Connect the Database to the Applications using the Mongoose....!
 const mongoose = require("mongoose");
-const { URI } = require("../Constants/links");
 
 //! 2. Connect with the Cluster.../ but this is not a good way to connect the database.
 const connectDB = async () => {
-  await mongoose.connect(URI);
+  await mongoose.connect(process.env.URI);
 };
 
 module.exports = connectDB;
